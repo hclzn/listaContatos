e3183a50b2579c040e2a64c62ebe15ecd300be27
Agata Colzani <agatacolzani@gmail.com>
2021-04-07 16:13:28 -0300

    No helmet at all

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ const MongoStore = require('connect-mongo'); //sessões salvas em base de dados
 const routes = require('./routes');
 const path = require('path');
 
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const csrf = require('csurf'); //sites externos não postam dentro da nossa aplicação
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middlewareTeste');
 
-app.use(helmet({
-    contentSecurityPolicy: false
-}));
+// app.use(helmet({
+//     contentSecurityPolicy: false
+// }));
 
 app.use(express.urlencoded({ extended: true })); //trata body das requisições, para não ficar undefined
 app.use(express.json());
